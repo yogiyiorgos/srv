4e05b2bd146af3e0b6e4294d05c97c58d13cae43
yogiyiorgos <georgefil@outlook.com>
2022-12-11 19:08:11 +0100

    ND01DEC11

--- withExpress/server.js
+++ withExpress/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const { logger } = require('./middleware/logEvents')
 const errorHandler = require('./middleware/errorHandler')
 const PORT = process.env.PORT || 3500
@@ -9,22 +10,6 @@ const PORT = process.env.PORT || 3500
 // custom middleware logger
 app.use(logger)
 
-// Cross Origin Resource Sharing
-const whitelist = [
-  'https://www.yoursite.com',
-  'http://127.0.0.1:5500',
-  'http://localhost:3500',
-]
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  optionsSuccessStatus: 200,
-}
 app.use(cors(corsOptions))
 
 // built-in middleware to handle urlencoded data
@@ -36,32 +21,10 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 // serve static files
-app.use(express.static(path.join(__dirname, '/public')))
+app.use('/', express.static(path.join(__dirname, '/public')))
 
-app.get('^/$|index(.html)?', (req, res) => {
-  // res.sendFile('./views/index.html', { root: __dirname })
-  res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
-
-app.get('/new-page(.html)?', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
-})
-
-app.get('/old-page(.html)?', (req, res) => {
-  res.redirect(301, '/new-page.html')
-})
-
-// Route handlers
-app.get(
-  '/hello(.html)?',
-  (res, req, next) => {
-    console.log('Attempted to load hello.html')
-    next()
-  },
-  (req, res) => {
-    res.send('Hello World')
-  }
-)
+app.use('/', require('./routes/root'))
+app.use('/emlpoyees', require('./routes/api/employees'))
 
 app.all('*', (req, res) => {
   res.status(404)
